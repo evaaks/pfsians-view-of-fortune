ea7deae28ebda2663405c149ef0f840b88b316cd
Liambda <evaaksonova@gmail.com>
2023-05-01 19:39:40 +0300

    всё

--- console.py
+++ console.py
@@ -49,50 +49,73 @@ class Console(Play):
 
     def move_cards(self):
         """
-        1. Перемещение из игрового ряда в базовый
-        2. Вывод сообщения, если карту нельзя переместить
-        3. Вывод сообщения, если пользователь ввел неверную цифру действия
-        4. Вывод сообщения об ошибке, если пользователь пытается переместить карту в несуществующую колонку(например в колонку 100)
-        5. Обработка ошибки если пользователь ввел не число
-        6. Передавать два аргумента в методы перемещения с игровыми рядами(индекс игрового ряда, индекс колонки в игровом ряду)
+        + 1. Перемещение из игрового ряда в базовый
+        + 2. Вывод сообщения, если карту нельзя переместить
+        + 3. Вывод сообщения, если пользователь ввел неверную цифру действия
+        + 4. Вывод сообщения об ошибке, если пользователь пытается переместить карту в несуществующую колонку(например в колонку 100)
+        + 5. Обработка ошибки если пользователь ввел не число
+        + 6. Передавать два аргумента в методы перемещения с игровыми рядами(индекс игрового ряда, индекс колонки в игровом ряду)
         7. Куда-то прикрутить метод win
         """
-        var = int(input("Choose number of operation for card movement from/to:\n"
-                        "1: deck/play row\n"
-                        "2: deck/bin\n"
-                        "3: deck/base row\n"
-                        "4: bin/play row\n"
-                        "5: bin/base row\n"
-                        "6: play row/ play row\n"
-                        ))
+        try:
+            var = int(input("Choose number of operation for card movement from/to:\n"
+                            "1: deck/play row\n"
+                            "2: deck/bin\n"
+                            "3: deck/base row\n"
+                            "4: bin/play row\n"
+                            "5: bin/base row\n"
+                            "6: play row/ play row\n"
+                            "7: play row/ base row\n"
+                            ))
 
-        result = True
-        if var == 1:
-            pos1 = int(input("Write coordinates to which row, column you want move your card"))
-            result = self.deck_play(pos1)
+            result = True
+            if var == 1:
+                row = int(input("Write coordinates to which row you want move your card: "))
+                col = int(input("Write coordinates to column you want move your card: "))
+                result = self.deck_play(row - 1, col - 1)
 
-        if var == 2:
-            result = self.deck_bin()
+            if var == 2:
+                result = self.deck_bin()
 
-        if var == 3:
-            pos1 = int(input("Write to which row, column you want move your card"))
-            result = self.deck_base(pos1)
+            if var == 3:
+                pos1 = int(input("Write to which row, column you want move your card: "))
+                result = self.deck_base(pos1)
 
-        if var == 4:
-            row = int(input("Write to which row you want move your card"))
-            col = int(input("Write to which column you want move your card"))
-            result = self.bin_play(row - 1, col - 1)
+            if var == 4:
+                row = int(input("Write to which row you want move your card: "))
+                col = int(input("Write to which column you want move your card: "))
+                result = self.bin_play(row - 1, col - 1)
 
-        if var == 5:
-            pos1 = int(input("Write to column you want move your card"))
-            result = self.bin_base(pos1)
+            if var == 5:
+                pos1 = int(input("Write to column you want move your card: "))
+                result = self.bin_base(pos1)
 
-        if var == 6:
-            pos1 = int(input("Write coordinates from which row, column to which row, column you want move your card"))
-            result = self.play_play(pos1)
+            if var == 6:
+                row = int(input("Write coordinates to which row you want move your card: "))
+                col = int(input("Write coordinates to column you want move your card: "))
+                result = self.play_play(row - 1, col - 1)# должно принимать 4 переменные,
+                #                                        из которых 2 первые - это место положение карты,
+                #                                       от куда мы хотим переместить. Аналогично с остальными функциями
 
+            if var == 7:
+                pos1 = int(input("Write coordinates from which row, column to which column you want move your card: "))
+                result = self.play_base(pos1)
+
+            elif var not in range(1, 8):
+                print(f"There is no operation: {var}\n"
+                      f"Chose another one from the list;)")
+
+        except ValueError:
+            print('Enter the coordinates of the position you want your card to move to,\n'
+                  'using a numerical value for the destination.')
+        except IndexError:
+            print(f"IndexError: Index is out of range.")
         if not result:
-            print("Wrong choose")
+            print("Please, check that:\n"
+                  "- you chose the appropriate position for card move\n"
+                  "- you put your card in an appropriate position and \n "
+                  "the logic of card's sequence monotonically increase still work\n"
+                  )
 
     def play(self):
         while self._in_play:
